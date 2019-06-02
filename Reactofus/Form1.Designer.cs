86704c71b0740c78eda0da381d77ee283d8cad5d
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-06-02 12:42:15 +0300

    Add UI stubs

--- Reactofus/Form1.Designer.cs
+++ Reactofus/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageRamDisk = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbAvailableDevices = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cbPause = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPageRamDisk);
+            this.tabControl1.Location = new System.Drawing.Point(12, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(392, 331);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageRamDisk
+            // 
+            this.tabPageRamDisk.Location = new System.Drawing.Point(4, 22);
+            this.tabPageRamDisk.Name = "tabPageRamDisk";
+            this.tabPageRamDisk.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageRamDisk.Size = new System.Drawing.Size(384, 305);
+            this.tabPageRamDisk.TabIndex = 0;
+            this.tabPageRamDisk.Text = "RamDisk ISO";
+            this.tabPageRamDisk.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(159, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reactofus Logo Here";
+            // 
+            // cbAvailableDevices
+            // 
+            this.cbAvailableDevices.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbAvailableDevices.FormattingEnabled = true;
+            this.cbAvailableDevices.Location = new System.Drawing.Point(12, 45);
+            this.cbAvailableDevices.Name = "cbAvailableDevices";
+            this.cbAvailableDevices.Size = new System.Drawing.Size(392, 21);
+            this.cbAvailableDevices.TabIndex = 1;
+            this.cbAvailableDevices.Text = "Choose USB device";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(325, 409);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start (stop)";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // cbPause
+            // 
+            this.cbPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbPause.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbPause.Location = new System.Drawing.Point(289, 409);
+            this.cbPause.Name = "cbPause";
+            this.cbPause.Size = new System.Drawing.Size(30, 23);
+            this.cbPause.TabIndex = 4;
+            this.cbPause.Text = "| |";
+            this.cbPause.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbPause.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(416, 444);
+            this.Controls.Add(this.cbPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbAvailableDevices);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reactofus";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageRamDisk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbAvailableDevices;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox cbPause;
     }
 }
 
--- Reactofus/Properties/AssemblyInfo.cs
+++ Reactofus/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Reactofus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ReactOS to USB deployer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Reactofus")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyrightn't © Ukrainian Government 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
